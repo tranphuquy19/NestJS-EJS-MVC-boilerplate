f0e2887ffb36d5992a0e3975bcba9ea83eb1dabb
tranphuquy19 <christian@enclave.vn>
2021-06-08 22:11:06 +0700

    add api version config

--- src/config/application/index.ts
+++ src/config/application/index.ts
@@ -1,6 +1,7 @@
 export const NODE_ENV = process.env.NODE_ENV;
 
 export const apiUrl = process.env.API_URL;
+export const fullApiUrl = `${apiUrl}/${process.env.API_VERSION}`;
 
 export const sessionSecret = process.env.SESSION_SECRET;
 export const sessionMaxAge = +process.env.SESSION_MAX_AGE;

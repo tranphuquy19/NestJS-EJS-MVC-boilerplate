ef457688ef5de0524856d6a6376feb1f89802c07
tranphuquy19 <christian@enclave.vn>
2021-07-13 03:20:42 +0700

    remove unused code

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -5,33 +5,7 @@ import { UserRepository } from './repositories';
 
 @Injectable()
 export class UserService {
-    private readonly users: any[];
-
-    constructor(private readonly userRepository: UserRepository) {
-        this.users = [
-            {
-                id: 'd50cd1a4-2f4f-49b3-abe1-be46fb8f0505',
-                username: 'john',
-                password: '123123',
-                roles: ['CUSTOMER', 'ADMIN'],
-                pet: { name: 'alfred', picId: 1 },
-            },
-            {
-                id: '3d6805a7-0dda-4aeb-adbb-d4e30c9fd659',
-                username: 'chris',
-                password: '123123',
-                roles: ['CUSTOMER'],
-                pet: { name: 'gopher', picId: 2 },
-            },
-            {
-                id: '73607b1a-12da-4012-94a8-0f9dcf0aaee3',
-                username: 'maria',
-                password: '123123',
-                roles: ['CUSTOMER'],
-                pet: { name: 'jenny', picId: 3 },
-            },
-        ];
-    }
+    constructor(private readonly userRepository: UserRepository) {}
 
     async findByUsername(username: string): Promise<UserEntity> {
         return await this.userRepository.findOne({ username });

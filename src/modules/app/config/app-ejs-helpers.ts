f60154e5c311e00bb847fe788485380ebb5780b9
tranphuquy19 <christian@enclave.vn>
2021-07-19 23:50:58 +0700

    refactor(core): add EJS helpers object

--- src/modules/app/config/app-ejs-helpers.ts
+++ src/modules/app/config/app-ejs-helpers.ts
@@ -1,11 +1,14 @@
 import { NestExpressApplication } from '@nestjs/platform-express';
 
+const ejsHelpers = {
+    copyrightYear: (brandName: string) => {
+        const _brandName = !!brandName ? brandName : 'NestJS EJS MVC boilerplate';
+        return `<b>${_brandName}</b> &copy; ${new Date().getUTCFullYear()}</b>`;
+    },
+};
+
 export function configEjsHelpers(app: NestExpressApplication) {
-    app.setLocal(
-        'copyrightYear',
-        (brandName: string) =>
-            `<b>${
-                !!brandName ? brandName : 'NestJS EJS MVC boilerplate'
-            }</b> &copy; ${new Date().getUTCFullYear()}</b>`,
-    );
+    Object.keys(ejsHelpers).forEach((helperName: string) => {
+        app.setLocal(helperName, ejsHelpers[helperName]);
+    });
 }

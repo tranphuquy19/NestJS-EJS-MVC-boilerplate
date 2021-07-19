5a85a16d43cb9824011458d41ec9f48c073df77c
tranphuquy19 <christian@enclave.vn>
2021-07-19 23:53:03 +0700

    docs(core): add some comments EJS helper

--- src/modules/app/config/app-ejs-helpers.ts
+++ src/modules/app/config/app-ejs-helpers.ts
@@ -1,10 +1,13 @@
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 const ejsHelpers = {
-    copyrightYear: (brandName: string) => {
+    copyrightYear: (brandName: string): string => {
         const _brandName = !!brandName ? brandName : 'NestJS EJS MVC boilerplate';
         return `<b>${_brandName}</b> &copy; ${new Date().getUTCFullYear()}</b>`;
     },
+    // yourHelper: (args: string[]): any => { // define your EJS helper here!
+    //     return args.join('');
+    // }
 };
 
 export function configEjsHelpers(app: NestExpressApplication) {

8e563dd8a159260fa0eaf98fcf58e598962e3314
tranphuquy19 <christian@enclave.vn>
2021-07-23 02:03:08 +0700

    refactor: refactor the code to not use nested template literals

--- src/shared/decorators/api-versioning-controller.decorator.ts
+++ src/shared/decorators/api-versioning-controller.decorator.ts
@@ -6,8 +6,7 @@ export interface IControllerVersionOptions {
 }
 
 export function ApiVersioningController({ resourceName, versions }: IControllerVersionOptions) {
-    const _prefixes = versions.map(
-        (version) => `api/${version}${!!resourceName ? `/${resourceName}` : ''}`,
-    );
+    const _resourceName = !!resourceName ? `/${resourceName}` : '';
+    const _prefixes = versions.map((version) => `api/${version}${_resourceName}`);
     return applyDecorators(Controller(_prefixes));
 }

4e7f24e86457d8006112cf32f60bb38d83b3ed78
tranphuquy19 <christian@enclave.vn>
2021-08-29 12:37:46 +0700

    refactor: refactor js templates

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('push', (event) => {
     const data = event.data.json();
 
-    if (!!event.origin && event.origin !== 'http://localhost:4000' ) // Compliant
+    if (!!event.origin && event.origin !== 'http://localhost:4000') // Compliant
         return;
 
     self.registration.showNotification(data.title, {

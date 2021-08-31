f9d20e4cf836f922030af7d21540749e4f76e869
tranphuquy19 <christian@enclave.vn>
2021-08-31 20:32:03 +0700

    fix: fix github CodeQL

--- src/modules/file-uploader/file-uploader.controller.ts
+++ src/modules/file-uploader/file-uploader.controller.ts
@@ -1,5 +1,6 @@
-import { Post, UploadedFile } from '@nestjs/common';
+import { Post, Res, UploadedFile } from '@nestjs/common';
 import { ApiV1Controller, FileTypes, Uploader } from '@shared';
+import { Response } from 'express';
 
 @ApiV1Controller('uploader')
 export class FileUploaderController {
@@ -8,11 +9,11 @@ export class FileUploaderController {
         allowedFileTypes: [FileTypes.IMAGE, FileTypes.AUDIO, FileTypes.VIDEO],
         originalName: false,
         maxFileSize: '1 GiB',
-        fileName: 'file',
+        fileName: (file) => `${file.originalname}-${Date.now()}`,
         overwrite: true,
         destination: './public/uploads',
     })
-    uploaded(@UploadedFile() file: Express.Multer.File) {
-        return file;
+    uploaded(@UploadedFile() file: Express.Multer.File, @Res() res: Response) {
+        return res.json(file);
     }
 }
